636736f013b2f6e87327397700032ae15a71ccd6
RoaaA <RoaaA@DESKTOP-OH14MPH>
2021-06-22 10:07:30 +0300

    Add project files.

---
+++ website7/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace website7.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Events()
+        {
+            return View();
+        }
+        public ActionResult Booking()
+        {
+            return View();
+        }
+        public ActionResult information()
+        {
+            return View();
+        }
+    }
+}
---
+++ website7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="website7.MvcApplication" Language="C#" %>
