86164127da0f918f3f2d102a2931f0897994869f
rawa alanazi <RoaaAlketban@outlook.sa>
2021-06-29 07:15:58 +0300

    Draft3

--- website7/Controllers/HomeController.cs
+++ website7/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using website7.Models;
 
 namespace website7.Controllers
 {
     public class HomeController : Controller
     {
+        int rem;
         // GET: Home
         public ActionResult Home()
         {
@@ -25,5 +27,30 @@ namespace website7.Controllers
         {
             return View();
         }
-    }
+        public ActionResult testDB()
+        {
+
+            eventsDBEntities db = new eventsDBEntities();
+            EventBooking eventBooking = db.EventBookings.SingleOrDefault(x => x.eventID == 1);
+
+            EventBookingView eventvm = new EventBookingView();
+            eventvm.eventID = eventBooking.eventID;
+            eventvm.eventName = eventBooking.eventName;
+            eventvm.remainingSeats = eventBooking.remainingSeats;
+            eventvm.remainingSeats = eventvm.remainingSeats - 1;
+            rem = eventvm.remainingSeats - 1;
+
+
+
+            return View(eventvm);
+        }
+        public ActionResult testdbd()
+        {
+            rem = rem - 1;
+            return View();
+
+
+        }
+
+        }
 }
---
+++ website7/Models/EventBookingView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace website7.Models
+{
+    public class EventBookingView
+    {
+
+        public int eventID { get; set; }
+        public string eventName { get; set; }
+        public int remainingSeats { get; set; }
+
+
+    }
+}
